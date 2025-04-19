27023f3addf9b016e1905e71c18722ea6cae745e
ivan <ivanmontanesfilipit@gmail.com>
2025-04-19 05:38:32 -0300

    punto terminado

--- Todo.c
+++ Todo.c
@@ -6,7 +6,7 @@
 //constantes que se utilizan en el programa//
 #define MAX_TAMA 70
 #define NOMBRE_PENDIENTES "LISTA PENDIENTES"
-#define NOMBRE_REALIZADS "NOMBRE REALIZADAS"
+#define NOMBRE_REALIZADS "LISTA REALIZADAS"
 //variable para el id autoincremental de las tareas//
 int id = 1000;
 
@@ -28,9 +28,13 @@ void mostrarTarea(Tarea tarea);
 //operaciones de la lista//
 Nodo * crearListaVacia();
 Nodo * crearNodo(Tarea tarea);
-void insertarNodo(Nodo ** start, Nodo * nodo);
-Nodo * quitarNodo(Nodo ** start, int idBuscado);
+void insertarNodo(Nodo ** lista, Nodo * nodo);
+Nodo * quitarNodo(Nodo ** lista, int idBuscado);
 bool mostrarLista(Nodo * lista, char * NombreLista);
+void consultarTareas(Nodo * listaPendientes, Nodo * listaRealizadas);
+void recorrerYmostrarSegunCriterio(Nodo * listaPendientes, Nodo * listaRealizadas, int criterio);
+//liberar memoria//
+void liberarMemoria(Nodo * lista);
 int main(){
     //creamos las listas//
     Nodo * listaPendientes = crearListaVacia();
@@ -59,8 +63,7 @@ int main(){
         printf("1: MOSTRAR TAREAS PENDIENTES\n");
         printf("2: MOSTRAR TAREAS REALIZADAS\n");
         printf("3: MARCAR TAREA COMO REALIZADA\n");
-        printf("4: BUSCAR TAREA POR ID\n");
-        printf("5: BUSCAR TAREA POR PALABRA CLAVE\n");
+        printf("4: CONSULTAR TAREAS\n");
         printf("OTRO PARA SALIR\n");
         fflush(stdin);
         scanf("%d",&opcion);
@@ -91,10 +94,20 @@ int main(){
                 }else{
                     printf("no hay ninguna tarea pendiente\n\n");
                 }
-                
             break;
+            
+            case 4:
+                consultarTareas(listaPendientes,listaRealizadas);
+            break;
+
+            default: seguir = false;
         }
     }while(seguir);
+
+    //liberamos memoria//
+    liberarMemoria(listaPendientes);
+    liberarMemoria(listaRealizadas);
+    return 0;
 }
 
 //funciones con las tarea//
@@ -130,6 +143,7 @@ Tarea crearTarea()
     return tarea;
 }
 
+//mostrar tarea//
 void mostrarTarea(Tarea tarea)
 {   
     printf("----------TAREA----------\n");
@@ -153,14 +167,14 @@ Nodo * crearNodo(Tarea tarea)
     return Tnodo;
 }
 
-void insertarNodo(Nodo ** start, Nodo * nodo)
+void insertarNodo(Nodo ** lista, Nodo * nodo)
 {
-    nodo->Siguiente = *start;
-    *start = nodo;
+    nodo->Siguiente = *lista;
+    *lista = nodo;
 }
 
-Nodo * quitarNodo(Nodo ** start, int idBuscado){
-    Nodo ** aux = start;
+Nodo * quitarNodo(Nodo ** lista, int idBuscado){
+    Nodo ** aux = lista;
 
     while (*aux && (*aux)->T.TareaID != idBuscado){
         aux = &(*aux)->Siguiente;
@@ -176,8 +190,25 @@ Nodo * quitarNodo(Nodo ** start, int idBuscado){
     return NULL;
 }
 
+void consultarTareas(Nodo * listaPendientes, Nodo * listaRealizadas)
+{
+    int opcion;
+    printf("----------CONSULTAR TAREAS----------\n");
+    do{
+        printf("1: Consultar tarea por id\n");
+        printf("2: Consultar tarea por Palabra Clave\n");
+        printf("ELIGA UNA OPCION:");
+        fflush(stdin);
+        scanf("%d",&opcion);
+    }while(!(opcion == 1 || opcion == 2));
+    //variable para recibir los nodos que retornen las funciones//
+    Nodo * nodoRetornado;
+    recorrerYmostrarSegunCriterio(listaPendientes,listaRealizadas,opcion);
+}
+
 bool mostrarLista(Nodo * lista, char * NombreLista)
 {
+    //uso booleanos para cuando se quiera marcar una tarea como realizada y la lista este vacia retornar false y saber que no hay tareas pendientes//
     printf("----------%s----------\n",NombreLista);
     if(lista){
         while(lista){
@@ -189,4 +220,76 @@ bool mostrarLista(Nodo * lista, char * NombreLista)
         return false;
     }
     return true;
+}
+
+void recorrerYmostrarSegunCriterio(Nodo * listaPendientes, Nodo * listaRealizadas, int critero)
+{
+    //variable que se utilizara para indicar si no se encontro ninguna tarea//
+    bool seEncontro = false;
+    //funcionalidad por si se elige buscar la tarea por id//
+    if(critero == 1){
+        //pedimos al usuario que ingrese el id//
+        int idBuscado;
+        printf("ingrese el id:");
+        fflush(stdin);
+        scanf("%d",&idBuscado);
+        //recorremos la lista buscando el id, si se encuentra se indica que estamos en la lista de pendientes//
+        while(listaPendientes){
+            if(listaPendientes->T.TareaID == idBuscado){
+                printf("\nTAREA ENCONTRADA EN PENDIENTES\n");
+                mostrarTarea(listaPendientes->T);
+                seEncontro = true;
+            }
+            listaPendientes = listaPendientes->Siguiente;
+        }
+        //recorrermos la lista buscando el id, si se encuentra se indica que estamos en la lista de realizadas//
+        while(listaRealizadas){
+            if(listaRealizadas->T.TareaID == idBuscado){
+                printf("\nTAREA ENCONTRADA EN REALIZADAS\n");
+                mostrarTarea(listaRealizadas->T);
+                seEncontro = true;
+            }
+            listaRealizadas = listaRealizadas->Siguiente;
+        }
+
+    }else{
+        //funcionalidad por si se elige buscar la tarea por palabra clave//
+        //pedimos al usuario que ingrese o las palabras//
+        char palabrasClaves[MAX_TAMA];
+        printf("ingrese la/las palabras claves:");
+        fflush(stdin);
+        gets(palabrasClaves);
+        //recorrermos la lista buscando la palabra clave, si se encuentra se indica que estamos en la lista de pendientes//
+        while(listaPendientes){
+            if(strstr(listaPendientes->T.Descripcion,palabrasClaves)){
+                printf("\nTAREA ENCONTRADA EN PENDIENTES\n");
+                mostrarTarea(listaPendientes->T);
+                seEncontro = true;
+            }
+            listaPendientes = listaPendientes->Siguiente;
+        }
+        //recorrermos la lista buscando la palabra clave, si se encuentra se indica que estamos en la lista de realizadas//
+        while(listaRealizadas){
+            if(strstr(listaRealizadas->T.Descripcion,palabrasClaves)){
+                printf("\nTAREA ENCONTRADA EN REALIZADAS\n");
+                mostrarTarea(listaRealizadas->T);
+                seEncontro = true;
+            }
+            listaRealizadas = listaRealizadas->Siguiente;
+        }
+    }
+    if(!seEncontro){
+        printf("\nno se encontro la tarea en ninguna lista\n");
+    }
+}
+
+//liberar memoria//
+void liberarMemoria(Nodo * lista)
+{
+    while(lista){
+        Nodo * aux = lista;
+        lista = lista->Siguiente;
+        free(aux->T.Descripcion);
+        free(aux);
+    }
 }
